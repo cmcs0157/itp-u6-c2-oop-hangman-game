986fa169c8a40ef9d7d45a092b3cc2c9128370f1
cmcs0157 <cmcs0157@gmail.com>
2018-04-11 07:50:57 +0000

    GuessAttempt class done

--- hangman/game.py
+++ hangman/game.py
@@ -3,16 +3,34 @@ from .exceptions import *
 
 
 class GuessAttempt(object):
-    pass
+    
+    def __init__(self, character, hit=None, miss=None):
+        self.character = character
+        self.hit = hit
+        self.miss = miss
+        if hit == True and miss == True:
+            raise InvalidGuessAttempt
+    
+    def is_hit(self):
+        return self.hit == True
+    
+    def is_miss(self):
+        return self.miss == True
 
 
 class GuessWord(object):
     
     def __init__(self, answer):
+        if not answer:
+            raise InvalidWordException
         self.answer = answer
+        self.masked = len(answer) * '*'
         
-    def perform_attempt(self):
-        pass
+        
+    def perform_attempt(self, character):
+        if len(character) > 1:
+            raise InvalidGuessedLetterException
+        
 
 
 class HangmanGame(object):
@@ -24,8 +42,9 @@ class HangmanGame(object):
         self.previous_guesses = []
         self.word = GuessWord(self.select_random_word(word_list))
     
-    def guess(self, letter):
+    def guess(self, character):
         pass
+        
         
     @classmethod    
     def select_random_word(cls, word_list):
