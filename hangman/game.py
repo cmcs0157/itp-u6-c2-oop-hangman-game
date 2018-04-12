3f748727586abbf5e4e05044dc80bc69767cc859
cmcs0157 <cmcs0157@gmail.com>
2018-04-12 23:52:30 +0000

    Working copy; no docstrings

--- hangman/game.py
+++ hangman/game.py
@@ -1,16 +1,15 @@
 import random
 from .exceptions import *
 
-
 class GuessAttempt(object):
     
-    def __init__(self, character, hit=False, miss=False):
+    def __init__(self, character, hit=None, miss=None):
+        if hit == True and miss == True:
+            raise InvalidGuessAttempt()
         self.character = character
         self.hit = hit
         self.miss = miss
-        if hit == True and miss == True:
-            raise InvalidGuessAttempt
-    
+        
     def is_hit(self):
         return self.hit == True
         
@@ -27,7 +26,7 @@ class GuessWord(object):
         
     def perform_attempt(self, character):
         if len(character) > 1:
-            raise InvalidGuessedLetterException
+            raise InvalidGuessedLetterException()
         
         if character.lower() in self.answer.lower():
             self.masked = list(self.masked)
@@ -42,19 +41,41 @@ class HangmanGame(object):
     WORD_LIST = ['rmotr', 'python', 'awesome']
     
     def __init__(self, word_list=WORD_LIST, number_of_guesses=5):
-        self.number_of_guesses = number_of_guesses
         self.remaining_misses = number_of_guesses
         self.previous_guesses = []
         self.word = GuessWord(self.select_random_word(word_list))
     
-    def guess(self, character):
-        pass
+    def is_won(self):
+        return self.word.answer == self.word.masked
+            
+    def is_lost(self):
+        return self.remaining_misses == 0
         
+    def is_finished(self):
+        return self.is_lost() or self.is_won()
+    
+    def guess(self, character):
+        character = character.lower()
+        
+        if self.is_finished():
+            raise GameFinishedException()
+        
+        attempt = self.word.perform_attempt(character)
+        self.previous_guesses.append(character)
+        if attempt.is_miss():
+            self.remaining_misses -= 1
+        
+        if self.is_won():
+            raise GameWonException()
+        
+        if self.is_lost():
+            raise GameLostException()
+        return attempt
+    
     @classmethod    
     def select_random_word(cls, word_list):
         try:
             return random.choice(word_list)
         except:
             raise InvalidListOfWordsException
-        
         
